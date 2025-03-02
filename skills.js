1c3dcb5a496c8cc60302b7dbc1ab9d1d035ca823
Lisa Radford (Kane) <114978153+LisaCharlotteKane@users.noreply.github.com>
2025-03-02 20:45:11 +0000

    Add skills file to repository

---
+++ skills.js
@@ -0,0 +1,28 @@
+function calculateNumbers(var1, var2)   
+git add skills.js
+{
+    return var1 + var2;
+}git commit -m "Copilot first commit"   
+git add skills.js   
+git add skills.js
+git commit -m "Add skills file to repository"
+git push origin main    git add skills.js
+git commit -m "Add skills file to repository"   
+git push origin main
+}git add skills.js
+git commit -m "Add skills file to repository"
+git push origin main
+}git add skills.js      
+git commit -m "Copilot first commit"    
+git push origin main
+}git add skills.js
+git commit -m "Add skills file to repository"
+git push origin main
+}git add skills.js
+git commit -m "Add skills file to repository"
+git push origin main
+}git add skills.js
+git commit -m "Add skills file to repository"
+git push origin main
+}git add skills.js
+git commit -m "Add skills file to repository"

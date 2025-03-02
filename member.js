078f78a15041be55ed70f16c9fac2de30b981c22
Lisa Radford (Kane) <114978153+LisaCharlotteKane@users.noreply.github.com>
2025-03-02 21:09:32 +0000

    Updated member.js

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember() {
+  const member = {
+    name: 'Lisa Radford',
+    email: 'lisacharlottekane@github.com',
+    location: 'Auckland,NZ',
+    skills: [
+      {
+        name: 'JavaScript',
+        level: 'advanced',
+        experience: '5 years',
+        interest: 'high',
+        projects: [
+          {
+            name: 'Project 1',
+            description: 'This is a project',
+            url: ''
+          }
+        ]
+      }
+    ]
+  };
+}
+// Removed git commands from the JavaScript file
